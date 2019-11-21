2d8d5dd8709697e3783f9f5015836a85ed1bcc0d
clattanoia <yacnlee@outlook.com>
2019-11-21 17:29:29 +0800

    delete shell

--- docker_build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-IMAGE_NAME=clattanoia/devops:latest
-CONTAINER_NAME=devops
-docker pull $IMAGE_NAME && docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true && docker run -d --name devops -p 8080:3000 $IMAGE_NAME
