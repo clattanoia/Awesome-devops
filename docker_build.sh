78018cfef05fc5d19e94c8b8033874ba998325ac
clattanoia <yacnlee@outlook.com>
2019-11-21 17:19:19 +0800

    update bash shell

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-docker pull clattanoia/devops:latest && docker stop devops && docker rm devops && docker run -d --name devops -p 8080:3000 clattanoia/devops:latest
+IMAGE_NAME=clattanoia/devops:latest
+CONTAINER_NAME=devops
+docker pull $IMAGE_NAME && docker stop $CONTAINER_NAME || true && docker rm $CONTAINER_NAME || true && docker run -d --name devops -p 8080:3000 $IMAGE_NAME
