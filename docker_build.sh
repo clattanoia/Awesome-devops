e6f113820b458713b3d2970e38c21784fce42a5a
clattanoia <yacnlee@outlook.com>
2019-11-22 16:34:28 +0800

    update .dockerignore

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-docker pull clattanoia/devops:latest && 
-docker stop devops || true && 
-docker rm devops || true && 
-docker run -d --name devops -p 8080:3000 clattanoia/devops:latest
+IMAGE_NAME="clattanoia/devops:latest"
+CONTAINER_NAME="devops"
+
+docker pull $IMAGE_NAME && 
+docker stop $CONTAINER_NAME || true && 
+docker rm $CONTAINER_NAME || true && 
+docker run -d --name devops -p 8080:3000 $IMAGE_NAME
