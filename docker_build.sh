c55bf6fa02bcbd992cbe0296adbf574c7505db62
clattanoia <yacnlee@outlook.com>
2019-11-25 15:19:28 +0800

    update shell

--- docker_build.sh
+++ docker_build.sh
@@ -6,4 +6,4 @@ CONTAINER_NAME="devops"
 docker pull $IMAGE_NAME && 
 docker stop $CONTAINER_NAME || true && 
 docker rm $CONTAINER_NAME || true && 
-docker run -d --name devops -p 8080:3000 $IMAGE_NAME
+docker run -d --name CONTAINER_NAME -p 8080:3000 $IMAGE_NAME
